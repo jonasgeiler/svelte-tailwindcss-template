826f52a8647f97aae88fd8477a7181849d40c31c
Skayo <jonas@skayo.dev>
2020-11-26 04:20:54 +0100

    Improved build upgraded to rollup-plugin-svelte@7.0.0, upgraded to tailwindcss@2.0.0, use script for inlining css and hashing filenames, removed service worker

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,34 @@
 module.exports = {
-  theme:    {
-    extend: {
-      // ...
-    },
-  },
-  variants: {},
-  purge:    {
-    content: ['./src/**/*.svelte', './src/**/*.html'],
-    options: {
-      whitelistPatterns: [/svelte-/],
-      
-      defaultExtractor: (content) => {
-        const regExp = new RegExp(/[A-Za-z0-9-_:/]+/g);
-        const matchedTokens = [];
-        let match = regExp.exec(content);
-        while (match) {
-          if (match[0].startsWith('class:')) {
-            matchedTokens.push(match[0].substring(6));
-          } else {
-            matchedTokens.push(match[0]);
-          }
-          match = regExp.exec(content);
-        }
-        return matchedTokens;
-      },
-    },
-  },
-  future:   {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault:         true,
-  },
-  plugins:  [],
+	theme:    {
+		extend: {
+			// ...
+		},
+	},
+	variants: {},
+	purge:    {
+		content: ['./src/**/*.svelte', './src/**/*.html'],
+		options: {
+			safelist: [/svelte-/],
+
+			defaultExtractor: (content) => {
+				const regExp = new RegExp(/[A-Za-z0-9-_:/]+/g);
+				const matchedTokens = [];
+				let match = regExp.exec(content);
+				while (match) {
+					if (match[0].startsWith('class:')) {
+						matchedTokens.push(match[0].substring(6));
+					} else {
+						matchedTokens.push(match[0]);
+					}
+					match = regExp.exec(content);
+				}
+				return matchedTokens;
+			},
+		},
+	},
+	future:   {
+		removeDeprecatedGapUtilities: true,
+		purgeLayersByDefault:         true,
+	},
+	plugins:  [],
 };
